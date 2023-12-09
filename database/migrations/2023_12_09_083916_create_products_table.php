9789a495053ee0406208780509efd46ecce4464e
innotuy <innotuyi67@gmail.com>
2023-12-09 15:12:56 +0200

    Adding migration

--- database/migrations/2023_12_09_083916_create_products_table.php
+++ database/migrations/2023_12_09_083916_create_products_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('description');
+            $table->string('photo1');
+            $table->string('photo2');
+            $table->string('photo3');
             $table->timestamps();
         });
     }
